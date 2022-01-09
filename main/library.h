71b62b3d84d981defba2d1068d5a35b75e6dcff4
HenriHTTP <Zegordoplays123@gmail.com>
2022-01-09 15:01:26 -0300

    create header and problem funcion

---
+++ main/library.h
@@ -0,0 +1,9 @@
+#ifndef LIBRARY_H_INCLUDED
+#define IBRARY_H_INCLUDED
+#define  Log_ "\n"
+
+
+int request_question();
+ 
+
+#endif
--- main/strig.cpp
+++ main/strig.cpp
@@ -2,28 +2,24 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
-
+#include "library.h"
 using namespace std;
 
-//*********************************************************************//
-//algorithm that takes username , prints queries and displays results // 
-//********************************************************************//
-int main()
-{
+int request_question();
+
+int main(){
+    
     string user ,password,confirm_password,confirm_user ;
-  
-    int resquest_question ();
-    int star_form();
-    int reboot();
+      
+cout<<Log_; 
 
 
-
-    cout<< "\ncreat a new user\n";
+    cout<< "creat a new user";
 
     cin >> user ; 
     system ("clear");
 
-    cout << "\ncriate a new password\n";
+    cout << "criate a new password";
     cin >> password;
 
     system ("clear");
@@ -32,18 +28,15 @@ int main()
     while (user != confirm_user)
     {
 
-        cout << "\nenter your user\n";
+        cout<< "enter your user";
         cin >> confirm_user; 
         system("clear");
-    
-     //********************//
-    //user authentication // 
-   //********************//
+ 
 
 
          if (user == confirm_user)
     {
-        cout<<" \ncorrect user\n";
+        cout<<"correct user";
         system("clear");
 
 
@@ -58,152 +51,33 @@ int main()
 
     while (password!=confirm_password)
     {
-        cout <<"\nenter your password\n";
+        cout<<"enter your password";
         cin >> confirm_password; 
         system("clear");
 
-
-     //************************//
-    //password authentication // 
-   //************************//
-
     if (confirm_password == password)
     {
-    cout << "\ncorrect password\n";
+    cout << "correct password";
     system("clear");
 
 
     }else{ 
 
-        cout << "\nerro 404\n";
+        cout << "erro 404";
         system ("clear");
 
     }
     }
 
-    cout << "\nlogin complete\n";
+    cout << "login complete";
     system("clear"); 
 
-    star_form();
+   
 
-
+   
     return 0;
 }
-     //*******************//
-    //funcion's questions// 
-   //*******************//
-
-int resquest_question (){
-
-    string question; 
-    int point = false;
-        
-        
-        cout << "\nwelcome to quiz\n";
-     
-     //*********************//
-    //start question's quiz// 
-   //*********************//
-        
-        cout<< "\nwhats is extension of c++ language\n";
-        cin >> question;
-
-        if(question=="cpp")
-        {
-            point++;
-            system("clear");
-        }else{
-            
-            system("clear");
-        }
-
-        question;
-
-        cout<< "\nwhats is extension of javascript language\n";
-        cin >> question;
-
-        if(question=="js")
-        {
-            point++;
-            system("clear");
-        }else{
-
-            system("clear");
-            
-        }
-        
-
-        question;
-     //*******************//
-    //end question's quiz// 
-   //*******************//
-
-       cout <<"\n  you haved "; 
-       cout << point;
-       cout <<"\n Congratulations\n";
-
     
-}
-
-
-     //*******************//
-    //funcion's star form// 
-   //*******************//
-
-
-int star_form (){
-
-    int reboot();
-
-
-
-    string resquest,star;
-
-    //************************************//
-    //star resquest star or resquest exit//
-    //**********************************//
-
-    cout << "\ndo you want to start the questionnaire\n";
-    cout << "\nwrite yes for continue  or no for exit\n";
-    cin >> resquest;
-    system("clear");
-        if (resquest=="yes")
-    {
-        int resquest_question();
-        resquest_question();
-    }else{
-
-        reboot();
-        
-
-    }
-    
-}
-
-     //**********************//
-    //funcion's kill program// 
-   //**********************//
-
-int reboot(){
-
-    string resquest_reboot; 
-
-    cout << "\ndo you exit program\n"; 
-    cout<< "\nwrite 'yes'\n";
-    cin >> resquest_reboot;
-
-
-    while (resquest_reboot!="yes")
-
-
-    if (resquest_reboot=="yes")
-    {
-        system("clear");
-        main();
-    }
-    
-
-}
 
 
 
