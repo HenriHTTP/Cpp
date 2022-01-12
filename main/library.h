731b665c05fe2e5b3e5c76b5e674aafea06dcaf2
HenriHTTP <Zegordoplays123@gmail.com>
2022-01-11 22:38:46 -0300

    create strucst and include main and clear code main

--- main/library.h
+++ main/library.h
@@ -6,20 +6,49 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
-
+#include <new> 
 using namespace std;
+ 
+ 
+
+
+struct quest{
+    
+    string request_new_writer;   
+
+
+};
+
+
+
+  
+
+
+
 
 int request_question(){
 
     string question; 
     int point = 0;
+     
+        quest *request_question = new quest [11]; 
+        request_question[0].request_new_writer =" welcome to quiz";
+        request_question[1].request_new_writer =" whats extension of c++ language ?";
+        request_question[2].request_new_writer =" whats extencion of javascript language ?";
+        request_question[3].request_new_writer =" correct aswers ";
+        request_question[4].request_new_writer =" welcome to quiz";
+        request_question[5].request_new_writer =" welcome to quiz";
+        request_question[6].request_new_writer =" welcome to quiz";
+        request_question[10].request_new_writer =" congratulations";
+
+
         
         
-        cout << "\nwelcome to quiz\n";
+        cout << Log_<<request_question[0].request_new_writer<<Log_;
      
 
         
-        cout<< "\nwhats is extension of c++ language\n";
+         cout << Log_<<request_question[1].request_new_writer<<Log_;
         cin >> question;
 
         if(question=="cpp")
@@ -33,7 +62,7 @@ int request_question(){
 
         question;
 
-        cout<< "\nwhats is extension of javascript language\n";
+        cout << Log_<<request_question[2].request_new_writer<<Log_;
         cin >> question;
 
         if(question=="js")
@@ -50,12 +79,93 @@ int request_question(){
         question;
 
 
-       cout <<"\n  you haved "; 
+        cout << Log_<<request_question[3].request_new_writer<<Log_;
        cout << point;
-       cout <<"\n Congratulations\n";
+       cout << Log_<<request_question[10].request_new_writer<<Log_;
         return 0;  
     
 }
+
+
+int star_user (){
+
+
+     string user ,password,confirm_password,confirm_user ;
+      
+
+
+        quest *request_writer =  new quest [10];
+        request_writer[0].request_new_writer= "creat new user";
+        request_writer[1].request_new_writer= "creat new password";
+        request_writer[2].request_new_writer= "confirm your user";
+        request_writer[3].request_new_writer= "correct user";
+        request_writer[4].request_new_writer= "Erro404(user not found)";
+        request_writer[5].request_new_writer= "confirm your password";
+        request_writer[6].request_new_writer= "Erro404(password not found)";
+        request_writer[7].request_new_writer= "complete login";
+
+
+    cout<<Log_<<request_writer[0].request_new_writer<<Log_;
+
+    cin >> user ; 
+    system ("clear");
+
+    cout <<Log_<< request_writer[1].request_new_writer<<Log_;
+    cin >> password;
+
+    system ("clear");
+
+
+    while (user != confirm_user)
+    {
+
+        cout<<Log_<< request_writer[2].request_new_writer<<Log_;
+        cin >> confirm_user; 
+        system("clear");
+ 
+
+
+         if (user == confirm_user)
+    {
+        cout<<Log_<<request_writer[3].request_new_writer<<Log_;
+        system("clear");
+
+
+    }else{
+
+        cout<<Log_<<request_writer[4].request_new_writer;
+        system("clear");
+
+    }
+
+    }
+
+    while (password!=confirm_password)
+    {
+        cout<<Log_<<request_writer[5].request_new_writer<<Log_;
+        cin >> confirm_password; 
+        system("clear");
+
+    if (confirm_password == password)
+    {
+    cout<<Log_ << "correct password"<<Log_;
+    system("clear");
+
+
+    }else{ 
+
+        cout<<Log_ <<request_writer[6].request_new_writer<<Log_;
+        system ("clear");
+
+    }
+    }
+
+    cout<<Log_ << request_writer[7].request_new_writer <<Log_;
+    system("clear"); 
+
+    return 0; 
+}
+
 
 
 #endif
--- main/strig.cpp
+++ main/strig.cpp
@@ -1,73 +1,12 @@
 
 #include "library.h"
 
-int request_question();
+
+
 
 int main(){
     
-    string user ,password,confirm_password,confirm_user ;
-      
-
-
-
-    cout<<Log_<< "creat a new user"<<Log_;
-
-    cin >> user ; 
-    system ("clear");
-
-    cout <<Log_<< "criate a new password"<<Log_;
-    cin >> password;
-
-    system ("clear");
-
-
-    while (user != confirm_user)
-    {
-
-        cout<<Log_<< "enter your user"<<Log_;
-        cin >> confirm_user; 
-        system("clear");
- 
-
-
-         if (user == confirm_user)
-    {
-        cout<<Log_<<"correct user"<<Log_;
-        system("clear");
-
-
-    }else{
-
-        cout<<"ERRO 404";
-        system("clear");
-
-    }
-
-    }
-
-    while (password!=confirm_password)
-    {
-        cout<<Log_<<"enter your password"<<Log_;
-        cin >> confirm_password; 
-        system("clear");
-
-    if (confirm_password == password)
-    {
-    cout<<Log_ << "correct password"<<Log_;
-    system("clear");
-
-
-    }else{ 
-
-        cout<<Log_ << "erro 404"<<Log_;
-        system ("clear");
-
-    }
-    }
-
-    cout<<Log_ << "login complet"<<Log_;
-    system("clear"); 
-
+    star_user(); 
    
     request_question(); 
    
